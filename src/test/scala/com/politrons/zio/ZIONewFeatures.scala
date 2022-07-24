90503ac5ce8db4ebac38adac9538a0213f5708a7
politrons <politrons@gmail.com>
2022-07-24 12:46:50 +0200

    multilayer

--- src/test/scala/com/politrons/zio/ZIONewFeatures.scala
+++ src/test/scala/com/politrons/zio/ZIONewFeatures.scala
@@ -13,6 +13,9 @@ class ZIONewFeatures {
    * than the use of Has[]
    * Now we just need to define the dependency in the [R,_,_] type, and use inside the program,
    * using ZIO.service[String] inside a for comprehension
+   *
+   * To be passed into the program we need to use the operator [provideLayer] in the program, and pass
+   * the value of the dependency.
    */
   @Test
   def layerFeatures(): Unit = {
@@ -31,5 +34,25 @@ class ZIONewFeatures {
     }
   }
 
+  @Test
+  def multipleLayerFeatures(): Unit = {
 
+    val zioProgram: ZIO[String with Int, Throwable, String] = for {
+      strValue <- ZIO.service[String]
+      intValue <- ZIO.service[Int]
+      newValue <- ZIO.succeed(s"${strValue.toUpperCase()} - ${intValue + 10}")
+    } yield newValue
+
+    val strDependency = ZLayer.succeed("hello zio world 2.0")
+    val intDependency = ZLayer.succeed(1981)
+
+    val dependency: ULayer[String with Int] = strDependency ++ intDependency
+
+    Unsafe.unsafe { implicit unsafe =>
+
+      val programWithDependencies = zioProgram.provideLayer(dependency)
+      val value = runtime.unsafe.run(programWithDependencies).getOrThrowFiberFailure()
+      println(s"Result $value")
+    }
+  }
 }
